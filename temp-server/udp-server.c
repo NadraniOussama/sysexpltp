3a764cdcc501e7969b5ba1c40d2d01e428d922ff
osboxes.org <osboxes@osboxes>
2021-01-31 11:40:52 -0500

    hello

---
+++ temp-server/udp-server.c
@@ -0,0 +1,83 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MAXLINE 1024
+
+int main(int argc, char * argv[])
+{
+  int sockfd, rec_sock, len;
+  struct sockaddr_in addr, recaddr;
+  char buffer[MAXLINE];
+  char *hello = "hello this the udp server\n==> at your service";
+
+  if (argc < 2) {
+    printf("Usage: %s port.\n",argv[0]);
+    exit(0);
+  }
+
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    perror(": Can't get socket");
+    exit(1);
+  }
+
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_family = AF_INET;
+  addr.sin_port = ((short)atoi(argv[1]));
+
+  if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    perror(": bind");
+    exit(1);
+  }
+
+  len = sizeof(addr);
+int n = recvfrom(sockfd,(char *)buffer,MAXLINE,MSG_WAITALL,
+      (struct sockaddr) &recaddr,&len);
+  if (getsockname(sockfd, (struct sockaddr *)&addr, &len) < 0)
+    {
+      perror(": can't get name");
+      _exit(1);
+   }
+  
+  printf("ip = %s, port = %d\n", inet_ntoa(addr.sin_addr), addr.sin_port);
+
+  if (listen(sockfd, 5) < 0) {
+    perror(": bind");
+    exit(1);
+  }
+
+  rec_sock = accept(sockfd, (struct sockaddr *)(&recaddr), &len);
+
+  if (rec_sock < 0) {
+    perror(": accept");
+    exit(1);
+  }
+
+  /* print the remote socket information */
+
+  printf("remote machine = %s, port = %x, %x.\n", inet_ntoa(recaddr.sin_addr), recaddr.sin_port, ntohs(recaddr.sin_port));
+  
+  memset(&recaddr, 0, sizeof(recaddr));
+  len = sizeof(addr);
+  if (getpeername(rec_sock, (struct sockaddr *)&recaddr, &len) < 0)
+    {
+      perror(": can't get name");
+      _exit(1);
+    }
+
+  printf("remote machine = %s, port = %d, %d.\n", inet_ntoa(recaddr.sin_addr), recaddr.sin_port, ntohs(recaddr.sin_port));
+  
+
+  write(rec_sock, "hi, there", 10);
+  sleep(20);
+  exit(1);
+}
+
+
+
+
